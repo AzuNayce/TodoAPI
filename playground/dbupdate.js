364025a598d99185968811d56a49711aea097c44
unknown <banoryann@gmail.com>
2018-10-08 14:41:13 +0400

    del + find + update

---
+++ playground/dbupdate.js
@@ -0,0 +1,34 @@
+// const mongoClient =  require('mongodb').MongoClient;
+
+const { MongoClient, ObjectId } = require('mongodb');
+
+// var User = {
+//     name:"Bob"
+// };
+
+// console.log(User.name);
+
+// var {name} = User;
+
+// console.log(name);
+
+MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, client) => {
+    if (err) {
+        return console.log(err);
+    }
+    console.log('connected');
+    const db = client.db('TodoApp');
+
+    db.collection('Todos').updateOne(
+        { _id: new ObjectId('5bbb0d11a84a6b0e2c30b7ff')},
+        {$set: {
+                completed: false
+            }
+        }).then((result) => {
+            console.log(JSON.stringify(result, undefined, 2));
+    }).catch((err) => {
+            console.log(err);
+    });
+
+    client.close();
+});
